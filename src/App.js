add3e6d2b43cd7d5ec6a8040e917bd2fb4527a01
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-27 18:34:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TinyMceEditor from './TinyMceEditor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h2>CKEditor 5 in React</h2>
+			<TinyMceEditor />
+		</div>
+	);
+};
 
 export default App;
